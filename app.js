e1e916466c39f55c3547a739f2cba4ee4d8d00c1
Matin De Wet <matindewet116@gmail.com>
2021-07-08 19:46:43 +0200

    Working solution
    this solution works perfectly, please change the dir. that you want, it need to be absolute

---
+++ app.js
@@ -0,0 +1,36 @@
+const extract = require('extract-zip')
+const fs = require('fs');
+const path = require('path');
+const zippedFiles = [];
+
+async function extractZip(source, target) {
+    try {
+        await extract(source, { dir: target });
+        console.log("Extraction complete");
+    } catch (err) {
+        console.log("Oops: extractZip failed", err);
+    }
+}
+
+const unzipFiles = async function(dirPath) {
+    const files = fs.readdirSync(dirPath);
+
+    await Promise.all(
+        files.map(async(file) => {
+            if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+                await unzipFiles(dirPath + "/" + file);
+            } else {
+                const fullFilePath = path.join(dirPath, "/", file);
+                const folderName = file.replace(".zip", "");
+                if (file.endsWith(".zip")) {
+                    zippedFiles.push(folderName);
+                    await extractZip(fullFilePath, path.join(dirPath, "/", folderName));
+                    await unzipFiles(path.join(dirPath, "/", folderName));
+                }
+            }
+        })
+    );
+};
+
+//extractZip("D:/MatinPersonalStudyProjects/RecursiveUnzipper/Source/test1.zip", "D:/MatinPersonalStudyProjects/RecursiveUnzipper/Target");
+unzipFiles("D:/MatinPersonalStudyProjects/RecursiveUnzipper/Source");
